c2a523488513eba8dff6698445c8e31255af41a7
S Ravi Kumar <techierathore@gmail.com>
2019-07-24 20:47:24 +0530

    Add project files.

---
+++ TRWebBlogEx/Controllers/SummerNoteExController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using TrWebBlogEx.Models;
+
+namespace TrWebBlogEx.Controllers
+{
+    public class SummerNoteExController : Controller
+    {
+        // GET: SummerNoteEx
+        public ActionResult NoteEntry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NoteEntry(NoteData aNoteData)
+        {
+            return View("ShowNote", aNoteData);
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult UploadFile(HttpPostedFileBase aUploadedFile)
+        {
+            string sImageName = string.Empty;
+            var vReturnImagePath = string.Empty;
+            if (aUploadedFile.ContentLength > 0)
+            {
+                var vFileName = Path.GetFileNameWithoutExtension(aUploadedFile.FileName);
+                var vExtension = Path.GetExtension(aUploadedFile.FileName);
+
+                sImageName = vFileName + DateTime.Now.ToString("YYYYMMDDHHMMSS");
+
+                var vImageSavePath = Server.MapPath("/UpImages/") + sImageName + vExtension;
+                 //sImageName = sImageName + vExtension;
+                 vReturnImagePath =  "/UpImages/" + sImageName + vExtension;
+                ViewBag.Msg = vImageSavePath;
+                var path = vImageSavePath;
+
+                // Saving Image in Original Mode
+                aUploadedFile.SaveAs(path);
+                var vImageLength = new FileInfo(path).Length;
+                //here to add Image Path to You Database ,
+                TempData["message"] = string.Format("Image was Added Successfully");
+            }
+            return Json(Convert.ToString(vReturnImagePath), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ TRWebBlogEx/Models/NoteData.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace TrWebBlogEx.Models
+{
+    /// <summary>
+    /// This is the Model Class used for showing the example of using 
+    /// free and Open source WYSWYG editer SummerNote in ASP.Net MVC 
+    /// Application
+    /// </summary>
+    public class NoteData
+    {
+        /// <summary>
+        /// For Title of the Note
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// To Save content of the NOte which can be anything.
+        /// </summary>
+        [AllowHtml]
+        public string Content { get; set; }
+    }
+}
